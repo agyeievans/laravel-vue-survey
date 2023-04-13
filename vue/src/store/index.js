348602ef27ab27cd9978005bc926b1d2a01aa60b
agyeievans <evanskofiagyei06@gmail.com>
2023-04-13 17:06:30 +0000

    added axios and working on login

--- vue/src/store/index.js
+++ vue/src/store/index.js
@@ -2,6 +2,8 @@ import {
   createStore
 } from "vuex"
 
+import axiosClient from "../axios";
+
 const store = createStore({
   state: {
     user: {
@@ -12,17 +14,18 @@ const store = createStore({
   getters: {},
   actions: {
     register({ commit }, user) {
-      return fetch(`http://localhost:8000/api/register`, {
-        headers: {
-          "Content-Type": "application/json",
-          Accept: "application/json",
-        },
-        method: "POST",
-        body: JSON.stringify(user),
-      }).then((res) => res.json()).then((res) => {
-        commit("setUser", res);
-        return res;
-      });
+      return axiosClient.post("/register", user)
+      .then(({data}) => {
+        commit('setUser', data);
+        return data;
+      })
+    },
+    login({ commit }, user) {
+      return axiosClient.post("/login", user)
+      .then(({data}) => {
+        commit('setUser', data);
+        return data;
+      })
     },
   },
   mutations: {
--- vue/src/views/Login.vue
+++ vue/src/views/Login.vue
@@ -13,18 +13,18 @@
       <router-link :to="{name: 'Register'}" class="font-medium text-indigo-600 hover:text-indigo-500">Register for free</router-link>
     </p>
   </div>
-  <form class="mt-8 space-y-6" action="#" method="POST">
+  <form class="mt-8 space-y-6" @submit="login">
     <input type="hidden" name="remember" value="true" />
     <div class="-space-y-px rounded-md shadow-sm">
       <div>
         <label for="email-address" class="sr-only">Email address</label>
-        <input id="email-address" name="email" type="email" autocomplete="email" required=""
+        <input id="email-address" name="email" type="email" autocomplete="email" required="" v-model="user.email"
           class="relative block w-full rounded-t-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
           placeholder="Email address" />
       </div>
       <div>
         <label for="password" class="sr-only">Password</label>
-        <input id="password" name="password" type="password" autocomplete="current-password" required=""
+        <input id="password" name="password" type="password" autocomplete="current-password" required="" v-model="user.password"
           class="relative block w-full rounded-b-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
           placeholder="Password" />
       </div>
@@ -32,7 +32,7 @@
 
     <div class="flex items-center justify-between">
       <div class="flex items-center">
-        <input id="remember-me" name="remember-me" type="checkbox"
+        <input id="remember-me" name="remember-me" type="checkbox" v-model="user.remember"
           class="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600" />
         <label for="remember-me" class="ml-2 block text-sm text-gray-900">Remember me</label>
       </div>
@@ -52,4 +52,26 @@
 
 <script setup>
 import { LockClosedIcon } from '@heroicons/vue/20/solid'
+import store from "../store"
+import {useRouter} from "vue-router"
+
+const router = useRouter();
+
+const user = {
+  email: '',
+  password: '',
+  remember: false
+}
+
+// define login function
+function login(ev) {
+  ev.preventDefault();
+  store
+  .dispatch('login', user)
+  .then(() => {
+    router.push({
+      name: 'Dashboard'
+    })
+  })
+}
 </script>
