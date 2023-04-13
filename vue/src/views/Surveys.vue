1197ffce5e5cba64650ccdfbd38dab2c3d26a133
agyeievans <evanskofiagyei06@gmail.com>
2023-04-13 14:09:52 +0000

    add pagecomponent

--- vue/src/views/Surveys.vue
+++ vue/src/views/Surveys.vue
@@ -1,16 +1,10 @@
+<template>
+  <PageComponent title="Surveys">
+    Content goes here
+  </PageComponent>
+</template>
+
 <script setup>
+import PageComponent from '../components/PageComponent.vue';
 </script>
 
-<template>
-  <header class="bg-white shadow">
-    <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-      <h1 class="text-3xl font-bold tracking-tight text-gray-900">Surveys</h1>
-    </div>
-  </header>
-  <main>
-    <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
-      <!-- Your content -->
-      <p>Content goes here</p>
-    </div>
-  </main>
-</template>
